ec91a01d7163fa971d0758440209858157108c2f
nateynate20 <57499322+nateynate20@users.noreply.github.com>
2025-07-16 22:04:39 -0400

    Add Profile model with team name only and update signup form

--- players/forms.py
+++ players/forms.py
@@ -1,22 +1,42 @@
-# nflpix/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from players.models import Profile
 
 class SignupUserForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(max_length=254, required=True, widget=forms.EmailInput(attrs={'class':'form-control'}))
-    phone_number = forms.CharField(max_length=15, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
     team_name = forms.CharField(max_length=15, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
-
 
     class Meta:
         model = User
-        fields = ('username','first_name', 'last_name','email','phone_number', 'team_name', 'password1', 'password2')
+        fields = ('username','first_name', 'last_name','email', 'team_name', 'password1', 'password2')
 
-def __init__(self, *args, **kwargs):
-    super(SignupUserForm, self).__init__(*args, **kwargs)
-    self.fields['username'].widget.attrs['class'] = 'form-control'
-    self.fields['password1'].widget.attrs['class'] = 'form-control'
-    self.fields['password2'].widget.attrs['class'] = 'form-control'
+    def __init__(self, *args, **kwargs):
+        super(SignupUserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already registered")
+        return email
+
+    def clean_team_name(self):
+        team_name = self.cleaned_data.get('team_name')
+        if Profile.objects.filter(team_name=team_name).exists():
+            raise forms.ValidationError("Team name already taken")
+        return team_name
+
+    def save(self, commit=True):
+        user = super(SignupUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+            profile = Profile.objects.create(user=user, team_name=self.cleaned_data['team_name'])
+        return user
--- players/models.py
+++ players/models.py
@@ -1,1 +1,9 @@
-# models.py
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    team_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
