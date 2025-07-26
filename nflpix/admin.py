933ec93e0329fa68d61ab3e59ac79ef0dac9bfc8
nateynate20 <57499322+nateynate20@users.noreply.github.com>
2025-07-26 17:41:22 -0400

    admin panel updates

--- nflpix/admin.py
+++ nflpix/admin.py
@@ -1,15 +1,8 @@
-# nflpix/admin.py
 from django.contrib import admin
 from .models import Player, LeagueCreationRequest
-from players.models import Profile  # <-- import from players app
-
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    extra = 0
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    inlines = [ProfileInline]
     list_display = ('username', 'email', 'team_name')
     search_fields = ('username', 'team_name', 'email')
     list_filter = ('team_name',)
@@ -33,6 +26,7 @@ class LeagueCreationRequestAdmin(admin.ModelAdmin):
         self.message_user(request, f"{updated} league(s) unapproved.")
     unapprove_leagues.short_description = "Mark selected leagues as unapproved"
 
+# Customize the admin site headers
 admin.site.site_header = "NFLPIX Admin"
 admin.site.site_title = "NFLPIX Admin Portal"
 admin.site.index_title = "Welcome to NFLPIX Admin"
