35d47ceb0702706e7e89e4689f2b9fe713095b88
nateynate20 <57499322+nateynate20@users.noreply.github.com>
2025-07-29 12:21:20 -0400

    updates to urls

--- nflpix/urls.py
+++ nflpix/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 from game_picks.views import display_nfl_schedule  # Import the view from nfl_schedule app
 from nflpix.views import landing_page
+from players.views import dashboard
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', landing_page, name='landing_page'),  # Use display_nfl_schedule from nfl_schedule app
     path('players/', include('django.contrib.auth.urls')),
     path('players/', include('players.urls')),
-    path('dashboard/', player_views.dashboard, name='dashboard'),
+    path('dashboard/', dashboard, name='dashboard'),
     path('game_picks/', include('game_picks.urls')),
     path('schedule/', include('nfl_schedule.urls')),  # Use urls from nfl_schedule app
 ]
