9a6d329f84c26418abcc6f5d8a8ea3e9e3f577af
nateynate20 <57499322+nateynate20@users.noreply.github.com>
2025-07-29 11:35:46 -0400

    updates to the views

--- players/views.py
+++ players/views.py
@@ -7,7 +7,6 @@ from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 
 
-
 @login_required
 def dashboard(request):
     user = request.user
@@ -21,6 +20,7 @@ def dashboard(request):
 def landing_page(request):
     return render(request, 'nflpix/landing.html')
 
+
 def login_user(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -29,12 +29,12 @@ def login_user(request):
             login(request, user)
             messages.success(request, f"Welcome back, {user.username}!")
             return redirect('dashboard')
-
         else:
             messages.error(request, "Invalid username or password.")
     else:
         form = AuthenticationForm()
     return render(request, 'registration/login.html', {'form': form})
+
 
 def signup(request):
     if request.method == "POST":
@@ -44,12 +44,12 @@ def signup(request):
             login(request, user)
             messages.success(request, "Account created successfully.")
             return redirect('dashboard')
-')
         else:
             messages.error(request, "Please correct the errors below.")
     else:
         form = SignupUserForm()
     return render(request, 'registration/signup.html', {'form': form})
+
 
 @require_POST
 def logout_user(request):
